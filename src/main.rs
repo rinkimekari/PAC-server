371d9e1f9530c9d15479b66d085aafdf1a8ebab5
rinkimekari <micahwasmuth@gmail.com>
2021-12-30 14:32:15 -0500

    fix stream handling

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::net::TcpStream;
 
 fn main() {
 
+    // NOTE: THIS IS VERY BAD CODE I WILL CHANGE THIS TO BE BETTER LATER
+    // THIS IS ONLY FOR TESTING PURPOSES RIGHT NOW
+
+    // TODO: send signal for stuff like closing stream
     // TODO: cli interface first
     // TODO: multithread
 
@@ -18,19 +22,25 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024]; // works better than string idk
+    loop {
+        let mut buffer = [0; 1024]; // works better than string idk
 
-    if let Ok(_) = stream.read(&mut buffer) {
-        let buffer = String::from_utf8_lossy(&buffer[..]);
-        println!("Request: {}", buffer);
+        if let Ok(_) = stream.read(&mut buffer) {
+            let buffer = String::from_utf8_lossy(&buffer[..]);
+            println!("Request: {}", buffer);
 
-        let prot_init = "PAC MESSAGE ->";
-        let response = if buffer.trim().starts_with(prot_init) {
-            "\nPAC SERVER OK\n"
-        } else {
-            "\nINVALID REQUEST\n"
-        };
-        stream.write(response.as_bytes()).unwrap();
-        println!("Sent response: {}", response);
+            let prot_init = "PAC MESSAGE ->";
+            let response = if buffer.trim().starts_with(prot_init) {
+                "\nPAC SERVER OK\n"
+            } else {
+                "\nINVALID REQUEST\n"
+            };
+
+            // TODO: make better
+            if let Err(_) = stream.write(response.as_bytes()) {
+                return;
+            }
+            println!("Sent response: {}", response);
+        }
     }
 }
